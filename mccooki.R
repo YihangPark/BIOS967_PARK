a75f9ba8bf0fde5200e8533e9a714ffc40656afe
dshizuka <dshizuka2@unl.edu>
2023-11-02 13:02:50 -0500

    Update mccooki.R

--- mccooki.R
+++ mccooki.R
@@ -1,5 +1,15 @@
+library(tidyverse)
+
 mccooki <- read.csv("data/mccooki.csv")
 mccooki
+
+## separate light and substrate variables
+mccooki = mccooki %>% 
+  mutate(light.condition=pull(mccooki, Cond..Lt.S.) %>% str_sub(start=1, end=1)) %>% 
+  mutate(substrate.condition=pull(mccooki, Cond..Lt.S.) %>% str_sub(start=3, end=3))
+
+mccooki
+
 model1 <- glm(Copsuccess ~ Fcond + Cond..Lt.S. + Fcond*Cond..Lt.S., family=binomial(link="logit"),data=mccooki)
 
 mccooki$Copsuccess = as.factor(mccooki$Copsucces)
