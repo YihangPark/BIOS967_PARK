408e6befe8bf79832d9764d4602fb44bb65a9ef1
YihangPark <park.yihang@gmail.com>
2023-11-02 12:48:02 -0500

    Update mccooki.R

--- mccooki.R
+++ mccooki.R
@@ -1,11 +1,9 @@
 mccooki <- read.csv("C:/Users/user/Documents/GitHub/BIOS967_PARK/data/mccooki.csv")
 mccooki
-model1 <- glm(Copsuccess ~ Fcond + Cond..Lt.S. + Fcond*Cond..Lt.S., family=binomial,data=mccooki, (link="logit"))
+model1 <- glm(Copsuccess ~ Fcond + Cond..Lt.S. + Fcond*Cond..Lt.S., family=binomial(link="logit"),data=mccooki)
 
 mccooki$Copsuccess = as.factor(mccooki$Copsucces)
 mccooki$Fcond = as.factor(mccooki$Fcond)
 mccooki$ Cond..Lt.S. = as.factor(mccooki$ Cond..Lt.S.)
 
-
-model1 <- glm(Copsuccess ~ Fcond + Cond..Lt.S. + Fcond*Cond..Lt.S., family=binomial,data=mccooki, (link="logit"))
-?glm
+summary(model1)
