c757d73326f4016325beeaf34e497f2fe40839af
dshizuka <dshizuka2@unl.edu>
2023-11-02 13:10:20 -0500

    Update mccooki.R

--- mccooki.R
+++ mccooki.R
@@ -4,13 +4,16 @@ mccooki <- read.csv("data/mccooki.csv")
 mccooki
 
 ## separate light and substrate variables
-mccooki = mccooki %>% 
+mccooki=mccooki %>% 
   mutate(light.condition=pull(mccooki, Cond..Lt.S.) %>% str_sub(start=1, end=1)) %>% 
-  mutate(substrate.condition=pull(mccooki, Cond..Lt.S.) %>% str_sub(start=3, end=3))
+  mutate(substrate.condition=pull(mccooki, Cond..Lt.S.) %>% str_sub(start=3, end=3)) %>%
+  mutate(cop.success=(X1st.cop.Time!="")+0)
 
 mccooki
 
-model1 <- glm(Copsuccess ~ Fcond + Cond..Lt.S. + Fcond*Cond..Lt.S., family=binomial(link="logit"),data=mccooki)
+model1 <- glm(cop.success~ Fcond + Cond..Lt.S. + Fcond*Cond..Lt.S., family=binomial(link="logit"),data=mccooki)
+summary(model1)
+anova(model1, test="Chisq") #to get overall effects of factors (without reference level) from glm, use the argument test="Chisq"
 
 mccooki$Copsuccess = as.factor(mccooki$Copsucces)
 mccooki$Fcond = as.factor(mccooki$Fcond)
